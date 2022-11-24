0190f8c9a3bf894516187c08290967d1fad021cb
Ian Naylan <42600121+i1d9@users.noreply.github.com>
2022-11-24 07:23:10 +0300

    Initial Commit

---
+++ essen/essenApp.swift
@@ -0,0 +1,17 @@
+//
+//  essenApp.swift
+//  essen
+//
+//  Created by Ian Nalyanya on 24/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct essenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
